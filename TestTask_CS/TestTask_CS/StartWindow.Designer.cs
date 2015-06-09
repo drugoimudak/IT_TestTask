0b83dff1f3a339daa37efc1e45968667a0156e79
Mikhail Stolovich <stolovich@yahoo.com>
2015-06-09 14:37:12 +0300

    Added combobox filling with unique items from the table.

--- TestTask_CS/TestTask_CS/StartWindow.Designer.cs
+++ TestTask_CS/TestTask_CS/StartWindow.Designer.cs
@@ -30,9 +30,6 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.db_Test_ITDataSet = new TestTask_CS.db_Test_ITDataSet();
-            this.dbTestITDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.orgDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.cityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -42,12 +39,24 @@
             this.totalDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dbordersBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.db_Test_IT_orders = new TestTask_CS.db_Test_IT_orders();
+            this.db_Test_ITDataSet = new TestTask_CS.db_Test_ITDataSet();
+            this.dbTestITDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.db_ordersTableAdapter = new TestTask_CS.db_Test_IT_ordersTableAdapters.db_ordersTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cb_date = new System.Windows.Forms.ComboBox();
+            this.cb_organization = new System.Windows.Forms.ComboBox();
+            this.cb_city = new System.Windows.Forms.ComboBox();
+            this.cb_country = new System.Windows.Forms.ComboBox();
+            this.cb_manager = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.db_Test_ITDataSet)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dbTestITDataSetBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dbordersBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.db_Test_IT_orders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_Test_ITDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbTestITDataSetBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
@@ -57,7 +66,6 @@
             this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.idDataGridViewTextBoxColumn,
             this.dateDataGridViewTextBoxColumn,
             this.orgDataGridViewTextBoxColumn,
             this.cityDataGridViewTextBoxColumn,
@@ -72,23 +80,6 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(617, 246);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // db_Test_ITDataSet
-            // 
-            this.db_Test_ITDataSet.DataSetName = "db_Test_ITDataSet";
-            this.db_Test_ITDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // dbTestITDataSetBindingSource
-            // 
-            this.dbTestITDataSetBindingSource.DataSource = this.db_Test_ITDataSet;
-            this.dbTestITDataSetBindingSource.Position = 0;
-            // 
-            // idDataGridViewTextBoxColumn
-            // 
-            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
-            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
-            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
-            this.idDataGridViewTextBoxColumn.ReadOnly = true;
             // 
             // dateDataGridViewTextBoxColumn
             // 
@@ -149,15 +140,120 @@
             this.db_Test_IT_orders.DataSetName = "db_Test_IT_orders";
             this.db_Test_IT_orders.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
+            // db_Test_ITDataSet
+            // 
+            this.db_Test_ITDataSet.DataSetName = "db_Test_ITDataSet";
+            this.db_Test_ITDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dbTestITDataSetBindingSource
+            // 
+            this.dbTestITDataSetBindingSource.DataSource = this.db_Test_ITDataSet;
+            this.dbTestITDataSetBindingSource.Position = 0;
+            // 
             // db_ordersTableAdapter
             // 
             this.db_ordersTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(645, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(645, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Organization";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(645, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(645, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 21);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Country";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(645, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 21);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Manager";
+            // 
+            // cb_date
+            // 
+            this.cb_date.FormattingEnabled = true;
+            this.cb_date.Location = new System.Drawing.Point(750, 8);
+            this.cb_date.Name = "cb_date";
+            this.cb_date.Size = new System.Drawing.Size(121, 29);
+            this.cb_date.TabIndex = 3;
+            // 
+            // cb_organization
+            // 
+            this.cb_organization.FormattingEnabled = true;
+            this.cb_organization.Location = new System.Drawing.Point(750, 48);
+            this.cb_organization.Name = "cb_organization";
+            this.cb_organization.Size = new System.Drawing.Size(121, 29);
+            this.cb_organization.TabIndex = 3;
+            // 
+            // cb_city
+            // 
+            this.cb_city.FormattingEnabled = true;
+            this.cb_city.Location = new System.Drawing.Point(750, 96);
+            this.cb_city.Name = "cb_city";
+            this.cb_city.Size = new System.Drawing.Size(121, 29);
+            this.cb_city.TabIndex = 3;
+            // 
+            // cb_country
+            // 
+            this.cb_country.FormattingEnabled = true;
+            this.cb_country.Location = new System.Drawing.Point(750, 148);
+            this.cb_country.Name = "cb_country";
+            this.cb_country.Size = new System.Drawing.Size(121, 29);
+            this.cb_country.TabIndex = 3;
+            // 
+            // cb_manager
+            // 
+            this.cb_manager.FormattingEnabled = true;
+            this.cb_manager.Location = new System.Drawing.Point(750, 191);
+            this.cb_manager.Name = "cb_manager";
+            this.cb_manager.Size = new System.Drawing.Size(121, 29);
+            this.cb_manager.TabIndex = 3;
             // 
             // StartWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(894, 272);
+            this.Controls.Add(this.cb_manager);
+            this.Controls.Add(this.cb_country);
+            this.Controls.Add(this.cb_city);
+            this.Controls.Add(this.cb_organization);
+            this.Controls.Add(this.cb_date);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Font = new System.Drawing.Font("Segoe UI Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -165,11 +261,12 @@
             this.Text = "Orders";
             this.Load += new System.EventHandler(this.StartWindow_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.db_Test_ITDataSet)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dbTestITDataSetBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dbordersBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.db_Test_IT_orders)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_Test_ITDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbTestITDataSetBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -181,7 +278,6 @@
         private db_Test_IT_orders db_Test_IT_orders;
         private System.Windows.Forms.BindingSource dbordersBindingSource;
         private db_Test_IT_ordersTableAdapters.db_ordersTableAdapter db_ordersTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn dateDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn orgDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn cityDataGridViewTextBoxColumn;
@@ -189,6 +285,16 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn managerDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn quantityDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn totalDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cb_date;
+        private System.Windows.Forms.ComboBox cb_organization;
+        private System.Windows.Forms.ComboBox cb_city;
+        private System.Windows.Forms.ComboBox cb_country;
+        private System.Windows.Forms.ComboBox cb_manager;
 
     }
 }
--- TestTask_CS/TestTask_CS/StartWindow.cs
+++ TestTask_CS/TestTask_CS/StartWindow.cs
@@ -21,8 +21,29 @@ namespace TestTask_CS
         {
             // TODO: This line of code loads data into the 'db_Test_IT_orders.db_orders' table. You can move, or remove it, as needed.
             this.db_ordersTableAdapter.Fill(this.db_Test_IT_orders.db_orders);
-            
+            FillComboBox("date",cb_date);
+            FillComboBox("city",cb_city);
+            FillComboBox("country",cb_country);
+            FillComboBox("manager",cb_manager);
+            FillComboBox("org", cb_organization);
         }
+
+        //this method fills choosen combobox from the order table with uniwue items from cb_items_name column
+        void FillComboBox(string cb_items_name,ComboBox cb_to_fill)
+        {
+            var dr = db_Test_IT_orders.db_orders;
+            int quantity = db_Test_IT_orders.db_orders.Rows.Count;//getting number of rows
+            cb_to_fill.Items.Clear();
+            for (int i = 0; i < quantity; i++)
+            {
+                if (!cb_to_fill.Items.Contains(dr[i][cb_items_name]))//adding only unique
+                {
+                    cb_to_fill.Items.Add(dr[i][cb_items_name]);
+                }
+            }
+        }
+
+       
 
         private void fillByToolStripButton_Click(object sender, EventArgs e)
         {
